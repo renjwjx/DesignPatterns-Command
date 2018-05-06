2a9b21417e8e279d8ab42e02461b372570fb2204
renjinwei <renjwjx@gmail.com>
2018-05-07 07:08:55 +0800

    Initial Commit

---
+++ DesignPatterns-Command/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DesignPatterns-Command
+//
+//  Created by jinren on 07/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
